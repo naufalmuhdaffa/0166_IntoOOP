e8db396cdedaa897687564c3f1b65267177fb7f0
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-04-23 11:35:52 +0700

    menambahkan kelas bangunDatar

--- OOP2/OOP2.cpp
+++ OOP2/OOP2.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
+class bangunDatar {
+
+//akses modifier
+private:
+    float panjang, Lebar;
+public:    
+    float Luas;
+
+    void input() { // methode input persegi panjang
+        cout << "Masukan Panjangnya = ";
+        cin >> panjang;
+        cout << "Masukan Lebarnya = ";
+        cin >> Lebar;
+    }
+    
+    float hitungLuas() {
+        return panjang * Lebar;
+    }
+    void display() {
+        cout << "Panjangnya = " << panjang << endl;
+        cout << "Lebarnya = " << Lebar << endl;
+        cout << "Luasnya = " << hitungLuas() << endl;
+    }
+};
+
 int main()
 {
     
